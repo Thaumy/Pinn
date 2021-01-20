9d59ef5ee3a91b39feb27c417955fb7567e5fdf2
Thaumy Cheng <Studio_ai@outlook.com>
2021-01-21 00:09:49 +0800

    fixed bugs

--- src/main/kotlin/Pinn/Ohayo.kt
+++ src/main/kotlin/Pinn/Ohayo.kt
@@ -27,13 +27,22 @@ object Ohayo {
         val calendar = Calendar.getInstance()
         val year = calendar.get(Calendar.YEAR)
         val month = calendar.get(Calendar.MONTH)
-        val day = calendar.get(Calendar.DAY_OF_MONTH)
+        var getUpDay = calendar.get(Calendar.DAY_OF_MONTH)
+        var goBedDay = calendar.get(Calendar.DAY_OF_MONTH)
 
         val getUpTime = Calendar.getInstance()
         val goBedTime = Calendar.getInstance()
 
-        getUpTime.set(year, month, day + 1, 7, 0)
-        goBedTime.set(year, month, day + 1, 23, 0)
+        //超出目标时间的日期补偿
+        if (LocalTime.now().hour >= 7) {
+            getUpDay++
+        }
+        if (LocalTime.now().hour >= 23) {
+            goBedDay++
+        }
+
+        getUpTime.set(year, month, getUpDay, 7, 0)
+        goBedTime.set(year, month, goBedDay, 23, 0)
 
         Timer().schedule(getUp, getUpTime.time, 24 * 60 * 60 * 1000)//早上好定时任务
         Timer().schedule(goBed, getUpTime.time, 24 * 60 * 60 * 1000)//晚安定时任务
